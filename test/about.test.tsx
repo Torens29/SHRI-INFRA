a907454ab09481feff1152a16c9f2aacf896733b
Torens29 <torens29@mail.ru>
2025-07-11 13:24:07 +0300

    test PR #8 (broken test(fixed))

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Vishnevskiy ');
+        expect(container.textContent).toBe('AboutAuthor: Vishnevskiy Roman');
     });
 });
